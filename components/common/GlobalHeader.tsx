2c9682ceccaae15dae337529e8a3c09da174a297
ak0007-code <asahi0107wave@gmail.com>
2023-06-05 20:51:42 +0900

    Initial Commit

---
+++ components/common/GlobalHeader.tsx
@@ -0,0 +1,12 @@
+import { GoThreeBars } from "react-icons/go";
+
+export const GlobalHeader = () => {
+  return (
+    <div className="flex items-center h-20">
+      <button>
+        <GoThreeBars className="text-4xl ml-4" />
+      </button>
+      <span className="ml-4 text-2xl font-semibold text-sky-900">UMAFUN</span>
+    </div>
+  );
+};
