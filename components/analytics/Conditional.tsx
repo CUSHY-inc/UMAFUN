d0d08f98f0ab9610297dcd45cf2195114b13e561
ak0007-code <asahi0107wave@gmail.com>
2023-06-10 16:15:47 +0900

    Conditionalコンポーネントを追加

---
+++ components/analytics/Conditional.tsx
@@ -0,0 +1,3 @@
+export const Conditional = () => {
+  return <div className="h-20 rounded-lg shadow mx-4"></div>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { Conditional } from "@/components/analytics/Conditional";
+
 export default function Page() {
-  return <></>;
+  return <Conditional />;
 }
