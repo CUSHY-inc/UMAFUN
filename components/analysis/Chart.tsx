5fcd01a94d8e0bee64c11c5d66dc5d9c3939b80a
ak0007-code <asahi0107wave@gmail.com>
2023-06-22 19:46:24 +0900

    チャートのコンポーネントを作成

---
+++ components/analysis/Chart.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { Chart } from "primereact/chart";
+import { data } from "autoprefixer";
+
+export const ResultChart = ({
+  labels,
+  datasets,
+}: {
+  labels: string[];
+  datasets: { label: string; data: number[] };
+}) => {
+  const [chartData, setChartData] = useState({});
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue("--text-color");
+    const textColorSecondary = documentStyle.getPropertyValue(
+      "--text-color-secondary"
+    );
+    const surfaceBorder = documentStyle.getPropertyValue("--surface-border");
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: datasets.label,
+          backgroundColor: documentStyle.getPropertyValue("--blue-500"),
+          borderColor: documentStyle.getPropertyValue("--blue-500"),
+          data: datasets.data,
+        },
+      ],
+    };
+    const options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        legend: {
+          labels: {
+            fontColor: textColor,
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+            font: {
+              weight: 500,
+            },
+          },
+          grid: {
+            display: false,
+            drawBorder: false,
+          },
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+      },
+    };
+    setChartData(data);
+    setChartOptions(options);
+  }, []);
+
+  return (
+    <div className="card">
+      <Chart type="bar" data={chartData} options={chartOptions} />
+    </div>
+  );
+};
--- components/analysis/ai/Table.tsx
+++ components/analysis/ai/Table.tsx
@@ -7,10 +7,151 @@ import { Table } from "react-daisyui";
 import { TbodyEx, TheadEx } from "@/boilerplate/components/Table";
 import { IResult } from "@/interfaces/analysis";
 
-const columns: ColumnDef<IResult>[] = [
+const createColumns = (data: IResult[]): ColumnDef<IResult>[] => [
   {
     accessorKey: "year",
     header: "開催年",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<string | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "arrive",
+    header: () => {
+      return <>着順</>;
+    },
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<number | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "frame",
+    header: "枠",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<number | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "number",
+    header: "馬番",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<number | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "name",
+    header: "馬名",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<string | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "genderOld",
+    header: "性齢",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<string | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "weight",
+    header: "斤量",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<number | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "jockey",
+    header: "騎手",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<string | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "time",
+    header: "タイム",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<number | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "margin",
+    header: "着差",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<string | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "popular",
+    header: "人気",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<number | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "odds",
+    header: "単勝オッズ",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<number | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "last",
+    header: "上り",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<number | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "lastRank",
+    header: "上り順",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<number | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+  {
+    accessorKey: "passing",
+    header: "通過",
+    enableSorting: false,
+    cell: ({ getValue }) => {
+      const value = getValue<string | null>();
+      return <td style={{ whiteSpace: "nowrap" }}>{value}</td>;
+    },
+  },
+];
+const columns: ColumnDef<IResult>[] = [
+  {
+    accessorKey: "year",
+    header: () => {
+      return <>開催年</>;
+    },
     enableSorting: false,
     cell: ({ getValue }) => {
       const value = getValue<string | null>();
